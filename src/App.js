e87ee5e847bd366555c1b9fc695d37daf114398a
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-11 17:48:59 +0100

    firt commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Miroslav greets the world.
         </p>
         <a
           className="App-link"
