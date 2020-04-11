580a98da7c3ca3a144278a5846a1c48a97cf7dfe
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-11 18:17:36 +0100

    buld folder added to git

---
+++ build/precache-manifest.98143868785a1b2bf13ba5902e8d1254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad64b3ab890b813864b66e329c999da5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c01bfc92a826f4052bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91ba892ce104250d6a55",
+    "url": "/static/js/2.4fd5bbd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4fd5bbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c01bfc92a826f4052bb",
+    "url": "/static/js/main.a50bf660.chunk.js"
+  },
+  {
+    "revision": "7234aae65e308a27c0f9",
+    "url": "/static/js/runtime-main.336845be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
