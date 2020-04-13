ffb3ae01219e59632b5c08555e378787b0197216
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-13 08:23:42 +0100

    Updates

---
+++ precache-manifest.65d4afc5646de9f4cb04f630ed132634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe3eba92d1689e4fe9fa117aed76d7f",
+    "url": "/MERN-ReactForExpress/index.html"
+  },
+  {
+    "revision": "81d88c8f19b7e4f36f8d",
+    "url": "/MERN-ReactForExpress/static/css/main.f9da2531.chunk.css"
+  },
+  {
+    "revision": "7dd4f7ad6ad71aa12374",
+    "url": "/MERN-ReactForExpress/static/js/2.472b72e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MERN-ReactForExpress/static/js/2.472b72e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d88c8f19b7e4f36f8d",
+    "url": "/MERN-ReactForExpress/static/js/main.7dfe085f.chunk.js"
+  },
+  {
+    "revision": "3e86507cdf29c4667831",
+    "url": "/MERN-ReactForExpress/static/js/runtime-main.defa0bf7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MERN-ReactForExpress/precache-manifest.65d4afc5646de9f4cb04f630ed132634.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MERN-ReactForExpress/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
