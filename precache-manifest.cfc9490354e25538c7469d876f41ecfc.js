dac80ba231dd85024c3dfd9cbbec5b6ab9fa1653
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-17 15:00:31 +0100

    Updates

--- precache-manifest.cfc9490354e25538c7469d876f41ecfc.js
+++ precache-manifest.cfc9490354e25538c7469d876f41ecfc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "351dfb4d9e745b02d36985b3d75c8f8f",
+    "revision": "0b326a5da3daa44d76b3702385719c8f",
     "url": "/MERN-ReactForExpress/index.html"
   },
   {
-    "revision": "d3acff4c8325119c6255",
+    "revision": "751b199fd34793858d57",
     "url": "/MERN-ReactForExpress/static/css/main.f9da2531.chunk.css"
   },
   {
-    "revision": "3e102382bb4c5a066635",
-    "url": "/MERN-ReactForExpress/static/js/2.fc9cbe62.chunk.js"
+    "revision": "c76e296838b07bf4a7d6",
+    "url": "/MERN-ReactForExpress/static/js/2.9452ae49.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/MERN-ReactForExpress/static/js/2.fc9cbe62.chunk.js.LICENSE.txt"
+    "url": "/MERN-ReactForExpress/static/js/2.9452ae49.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d3acff4c8325119c6255",
-    "url": "/MERN-ReactForExpress/static/js/main.ea9ca9fe.chunk.js"
+    "revision": "751b199fd34793858d57",
+    "url": "/MERN-ReactForExpress/static/js/main.8c5e6a16.chunk.js"
   },
   {
     "revision": "3e86507cdf29c4667831",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MERN-ReactForExpress/precache-manifest.e116faf6b2fe7ca8e313b77728910316.js"
+  "/MERN-ReactForExpress/precache-manifest.cfc9490354e25538c7469d876f41ecfc.js"
 );
 
 self.addEventListener('message', (event) => {
